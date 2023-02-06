c01fb45d8d0da6c851a6251693bf6b3c207eaa9f
Caio Espindola <caioespindola@hotmail.com>
2023-02-06 09:33:48 -0300

    print-infos-file

---
+++ print-infos.js
@@ -0,0 +1,10 @@
+const hero = {
+  name: 'Wolverine',
+  age: 160,
+  cpf: '07184792201',
+  email: 'loganw@hotmail.com'
+}
+
+console.log(`My name is ${hero.name} and i'm ${hero.age} years old`)
+
+console.log(hero.cpf.substring(0, 3))

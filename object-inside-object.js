dc39f518fa8d1d0964d2b3c40f95fc20930fe959
Caio Espindola <caioespindola@hotmail.com>
2023-02-07 09:25:56 -0300

    Object inside object

---
+++ object-inside-object.js
@@ -0,0 +1,19 @@
+const hero = {
+  name: 'Wolverine',
+  age: 160,
+  cpf: '07184792201',
+  email: 'loganw@hotmail.com'
+}
+
+// new object
+hero.rival = {
+  name: 'Saber Tooth',
+  age: 170,
+  cpf: '03087745602',
+  email: 'sabertooth@gmail.com'
+}
+
+// Changing an object
+hero.rival.name = 'Saber Tooth Tiger'
+
+console.log(hero)

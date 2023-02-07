52ccacf51adc9342c7df9e6c5e857356743cd1ce
Caio Espindola <caioespindola@hotmail.com>
2023-02-07 09:08:39 -0300

    Access an object using forEach

---
+++ access-phone-for-each.js
@@ -0,0 +1,11 @@
+const hero = {
+  name: 'Wolverine',
+  age: 160,
+  cpf: '07184792201',
+  email: 'loganw@hotmail.com',
+  phones: ['55899781421', '55977131469']
+}
+
+hero.phones.forEach(phone => {
+  console.log(phone)
+})

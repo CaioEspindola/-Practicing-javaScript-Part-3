c7a6d459e62e9fded52a3da8b5e3170d978ad7c9
Caio Espindola <caioespindola@hotmail.com>
2023-02-15 10:16:26 -0300

    fixing

--- literal-object.js
+++ literal-object.js
@@ -19,3 +19,30 @@ const displayName = display.bind(user)
 
 display()
 displayName()
+
+//Exemple 2:
+
+const user2 = {
+  name: 'Logan',
+  email: 'wolverine@hotmail.com',
+  birth: '1853/07/05',
+  role: 'estudante',
+  active: true,
+  printInfos: function () {
+    console.log(this.name, this.email)
+  }
+}
+
+const admin = {
+  nome: 'Charles Xavier',
+  email: 'xmanxavier@hotmail.com',
+  birth: '1943/08/17',
+  role: 'admin',
+  createClass() {
+    console.log('classroom created')
+  }
+}
+
+Object.setPrototypeOf(admin, user) //primeiro parametro é o objeto que VAI herdar, e segundo é quem vai passar a herança das propriedades do objeto
+admin.createClass()
+admin.printInfos()
